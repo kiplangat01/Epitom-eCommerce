a65bff8040471e6420a47e5b61146b11fbaaa281
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-06-24 04:18:26 +0300

    ft:deployment

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,7 @@ urlpatterns=[
     path('category-list',views.category_list,name='category-list'),
     path('brand-list',views.brand_list,name='brand-list'),
     path('product-list',views.product_list,name='product-list'),
+    path('category-product-list/<int:cat_id>',views.category_product_list,name='category-product-list'),
     
 ]
 
