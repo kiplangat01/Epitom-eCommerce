1f1867a85d8644c0ad94a433bae2906ca1035236
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-06-24 04:18:07 +0300

    ft:deployment

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
+
 from .models import Banner, Category, Brand, Color, Size, Product, ProductAttribute
 
-admin.site.register(Banner)
+
 admin.site.register(Brand)
 admin.site.register(Size)
+
+class BannerAdmin(admin.ModelAdmin):
+	list_display=('all_text','image_tag')
+admin.site.register(Banner,BannerAdmin)
 
 class ProductAdmin(admin.ModelAdmin):
     list_display=('id', 'title', 'brand', 'color', 'size', 'status', 'is_featured')
@@ -24,3 +29,5 @@ class ProductAttributeAdmin(admin.ModelAdmin):
     list_display=('id', 'product', 'price', 'color', 'size')
 
 admin.site.register( ProductAttribute,ProductAttributeAdmin)
+
+
