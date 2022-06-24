28f5bd284f8aeb27376458c7f4b39b81dbb796a7
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-06-24 04:18:16 +0300

    ft:deployment

--- main/models.py
+++ main/models.py
@@ -3,12 +3,18 @@ from django.utils.safestring import mark_safe
 
 # Banner
 class Banner(models.Model):
-    img=models.CharField(max_length=200)
+    img=models.ImageField(upload_to="banner_imgs/")
     all_text=models.CharField(max_length=300)
      
 
     class Meta:
         verbose_name_plural='1. Banners'
+    
+    def image_tag(self):
+        return mark_safe('<img src="%s" width="100"/>' % (self.img.url))
+
+    def __str__(self):
+        return self.all_text
 
 
 # category
